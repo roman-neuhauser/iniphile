76c8ae97b0d1ce8ed84728d79ecb73732c5e108f
neuhauser <devnull@localhost>
2010-03-11 20:45:28 +0100

    Makefile, msvc.mak: extracted common parts
    reducing duplication forces me to split the makefiles into
    several files and employ either inclusion (syntax differs
    among different makes) or a preprocessing step.  both have
    their upsides and downsides; I'm going to keep it make-only,
    and keep the trait of not requiring GNU make.
    
    * Makefile
    
      gone
    
    * bsd.mk
    
      entry point for compiling on FreeBSD (and the other family
      members, I believe)
    
    * gnu.mk
    
      entry point for compiling on GNU/Linux and other systems
      with GNU make
    
    * msvc.mak
    
      entry point for compiling with MSVC-9.0:
    
      nmake /fmsvc.mak
    
    * common-unix.mk
    * common.mk
    
      included as needed (using a make-implementation-dependent
      syntax) from the above files.

--- common-unix.mk
+++ common-unix.mk
@@ -1,3 +1,5 @@
+# vim: ts=8 noet ft=make
+
 PREFIX?=/usr/local
 LIBDIR?=$(PREFIX)/lib
 INCDIR?=$(PREFIX)/include/iniphile
@@ -38,36 +40,16 @@ LN_S?=ln -s
 RM_F?=rm -f
 INSTALL_PROGRAM?=$(INSTALL) -s
 
-VERSION_major=0
-VERSION_minor=1
-VERSIONSTRING=$(VERSION_major).$(VERSION_minor)
-
-
-COMPILE=$(CXX) $(CXXFLAGS) $(CXX_c) $(CXX_o)
 DLL_LINKAGE=
 
 LIBINIPHILE_PC=libiniphile.pc
-CANONICAL=libiniphile.so
 SONAME=$(CANONICAL).$(VERSION_major)
 IMPORT_LIB=$(SONAME)
-LIBOBJECTS=input.o output.o ast.o
-OBJECTS=initest-shared.o initest-static.o $(LIBOBJECTS)
-
-PUBLIC_HEADERS=ast.hpp astfwd.hpp declspec.hpp input.hpp metagram.hpp output.hpp
-
-ARTIFACTS=$(OBJECTS) initest-static$(dot_exe) initest-shared$(dot_exe) \
-	  *.exp *.lib *.manifest \
-	  $(SONAME) *.a *.pc
 
 EMBED_MANIFEST= \
 	:
 
 dot_exe=
-
-all: initest $(LIBINIPHILE_PC)
-
-clean:
-	$(RM_F) $(ARTIFACTS)
 
 check: initest
 	LD_LIBRARY_PATH=. ./initest-static$(dot_exe)
@@ -86,8 +68,6 @@ install: all
 	done
 	$(INSTALL) libiniphile.pc $(PKGCONFIGDIR)/libiniphile.pc
 
-initest: initest-static$(dot_exe) initest-shared$(dot_exe)
-
 libiniphile.pc: libiniphile.pc.in
 	trap "$(RM_F) libiniphile.pc.$$$$" EXIT; \
 	sed -e 's#@@PREFIX@@#$(PREFIX)#' \
@@ -95,35 +75,3 @@ libiniphile.pc: libiniphile.pc.in
 	    < libiniphile.pc.in \
 	    > libiniphile.pc.$$$$; \
 	mv libiniphile.pc.$$$$ libiniphile.pc
-
-$(SONAME): $(LIBOBJECTS)
-	$(LD) $(LDFLAGS) $(LDFLAGS_SO) $(LD_o)$(SONAME) $(LIBOBJECTS)
-
-libiniphile.a: $(LIBOBJECTS)
-	$(AR) $(ARFLAGS) $(AR_rc) libiniphile.a $(LIBOBJECTS)
-
-initest-static$(dot_exe): initest-static.o libiniphile.a
-	$(LD) $(LDFLAGS) $(LD_o)initest-static$(dot_exe) initest-static.o libiniphile.a $(LDLIBS)
-	$(EMBED_MANIFEST)
-
-initest-shared$(dot_exe): initest-shared.o
-	$(LD) $(LDFLAGS) $(LD_o)initest-shared$(dot_exe) initest-shared.o $(LDLIBS_SHARED) $(LDLIBS)
-	$(EMBED_MANIFEST)
-
-initest-static.o: initest.cpp
-	$(COMPILE)$@ initest.cpp
-
-initest-shared.o: initest.cpp
-	$(COMPILE)$@ initest.cpp $(DLL_LINKAGE)
-
-.cpp.o:
-	$(COMPILE)$@ $< $(DLL_LINKAGE)
-
-initest-shared$(dot_exe): $(IMPORT_LIB)
-initest-static.o: metagram.hpp input.hpp output.hpp ast.hpp
-initest-shared.o: metagram.hpp input.hpp output.hpp ast.hpp
-input.o: metagram.hpp input.hpp
-output.o: metagram.hpp output.hpp ast.hpp
-ast.o: metagram.hpp ast.hpp
-
-.PHONY: all check clean initest
--- msvc.mak
+++ msvc.mak
@@ -26,35 +26,16 @@ RM_F=del /f
 INSTALL_PROGRAM=$(INSTALL)
 INSTALL=copy
 
-VERSION_major=0
-VERSION_minor=1
-VERSIONSTRING=$(VERSION_major).$(VERSION_minor)
-
-COMPILE=$(CXX) $(CXXFLAGS) $(CXX_c) $(CXX_o)
 DLL_LINKAGE=-DINIPHILE_DLL
 
 LIBINIPHILE_PC=
-CANONICAL=libiniphile.so
 SONAME=libiniphile-$(VERSION_major).so
 IMPORT_LIB=libiniphile.lib
-LIBOBJECTS=input.o output.o ast.o
-OBJECTS=initest-shared.o initest-static.o $(LIBOBJECTS)
-
-PUBLIC_HEADERS=ast.hpp astfwd.hpp declspec.hpp input.hpp metagram.hpp output.hpp
-
-ARTIFACTS=$(OBJECTS) initest-static$(dot_exe) initest-shared$(dot_exe) \
-	  *.exp *.lib *.manifest \
-	  $(SONAME) *.a *.pc
 
 EMBED_MANIFEST= \
 	$(MT) $(MTFLAGS) -manifest $@.manifest -outputresource:$@;1
 
 dot_exe=.exe
-
-all: initest $(LIBINIPHILE_PC)
-
-clean:
-	$(RM_F) $(ARTIFACTS)
 
 check: initest
 	initest-static$(dot_exe)
@@ -71,39 +52,6 @@ install: all
 	for %f in ($(PUBLIC_HEADERS)) do \
 		$(INSTALL) %f $(DESTDIR)$(INCDIR)\%f
 
-initest: initest-static$(dot_exe) initest-shared$(dot_exe)
-
 $(IMPORT_LIB): $(SONAME)
 
-$(SONAME): $(LIBOBJECTS)
-	$(LD) $(LDFLAGS) $(LDFLAGS_SO) $(LD_o)$(SONAME) $(LIBOBJECTS)
-
-libiniphile.a: $(LIBOBJECTS)
-	$(AR) $(ARFLAGS) $(AR_rc)libiniphile.a $(LIBOBJECTS)
-
-initest-static$(dot_exe): initest-static.o libiniphile.a
-	$(LD) $(LDFLAGS) $(LD_o)initest-static$(dot_exe) initest-static.o libiniphile.a $(LDLIBS)
-	$(EMBED_MANIFEST)
-
-initest-shared$(dot_exe): initest-shared.o
-	$(LD) $(LDFLAGS) $(LD_o)initest-shared$(dot_exe) initest-shared.o $(LDLIBS_SHARED) $(LDLIBS)
-	$(EMBED_MANIFEST)
-
-initest-static.o: initest.cpp
-	$(COMPILE)$@ initest.cpp
-
-initest-shared.o: initest.cpp
-	$(COMPILE)$@ initest.cpp $(DLL_LINKAGE)
-
-.cpp.o:
-	$(COMPILE)$@ $< $(DLL_LINKAGE)
-
-initest-shared$(dot_exe): $(IMPORT_LIB)
-initest-static.o: metagram.hpp input.hpp output.hpp ast.hpp
-initest-shared.o: metagram.hpp input.hpp output.hpp ast.hpp
-input.o: metagram.hpp input.hpp
-output.o: metagram.hpp output.hpp ast.hpp
-ast.o: metagram.hpp ast.hpp
-
-.PHONY: all check clean initest
-.SUFFIXES: .o
+!include common.mk
